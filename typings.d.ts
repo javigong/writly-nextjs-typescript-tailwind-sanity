4582f029f8b7875a8cc48a4822552fc6bc463962
Javier Gongora <42308135+javigong@users.noreply.github.com>
2022-11-08 11:19:20 -0800

    Add Comment typings

--- typings.d.ts
+++ typings.d.ts
@@ -16,4 +16,21 @@ export interface Post {
     current: string;
   };
   body: string;
+  comments: Comment[];
+}
+
+export interface Comment {
+  _createdAt: string;
+  _id: string;
+  _rev: string;
+  _type: string;
+  _updatedAt: string;
+  approved: boolean;
+  comment: string;
+  email: string;
+  name: string;
+  post: {
+    _ref: string;
+    _type: string;
+  };
 }
