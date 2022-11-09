0a64f192637f42c8f0c4515a2b7629b8f21f7655
Javier Gongora <42308135+javigong@users.noreply.github.com>
2022-11-09 10:03:05 -0800

    Update Header.tsx

--- components/Header.tsx
+++ components/Header.tsx
@@ -19,8 +19,12 @@ const Header = (props: Props) => {
           </div>
         </Link>
         <div className="hidden md:inline-flex items-center space-x-5">
-          <h3>About</h3>
-          <h3>Contact</h3>
+          <Link href="/about">
+            <h3>About</h3>
+          </Link>
+          <Link href="/contact">
+            <h3>Contact</h3>
+          </Link>
           <h3 className="text-white bg-[royalblue] px-4 py-1 rounded-full">
             Follow
           </h3>
@@ -28,7 +32,9 @@ const Header = (props: Props) => {
       </div>
 
       <div className="flex items-center space-x-5 text-[royalblue]">
-        <h3>Sign In</h3>
+        <Link href="https://writly-nextjs.sanity.studio/">
+          <h3>Sign In</h3>
+        </Link>
         <h3 className="border rounded-full px-4 py-1 border-[royalblue]">
           Get Started
         </h3>
